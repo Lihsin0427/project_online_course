c36eaae772173228a1178c5a2a5303842eaa5729
lihsin <lihsin0427@gmail.com>
2022-02-13 00:46:28 +0800

    CRUD actions in courses_controller / views and partial render view of new, edit

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,6 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: [:edit, :update, :destroy]
+
   def index
     @courses = Course.all
   end
@@ -9,14 +11,36 @@ class CoursesController < ApplicationController
 
   def create
     @course = Course.new(course_params)
+
     if @course.save
-      redirect_to courses_path
+      redirect_to courses_path, notice: "課程新增成功!"
     else
       render :new
-    end  
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @course.update(course_params)
+      redirect_to courses_path, notice: "課程更新成功!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @course.destroy
+
+    redirect_to courses_path, notice: "課程已刪除!"
   end
 
   private
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
   def course_params
     params.require(:course).permit(:topic, :price, :currency, :category, :is_available, :url, :content, :validity_period)
   end

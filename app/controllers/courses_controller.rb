c894df262b9f4baa84f30fb32e4b0b01bdbe1122
lihsin <lihsin0427@gmail.com>
2022-02-13 00:22:59 +0800

    create course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,5 +6,18 @@ class CoursesController < ApplicationController
   def new
     @course = Course.new
   end
-  
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to courses_path
+    else
+      render :new
+    end  
+  end
+
+  private
+  def course_params
+    params.require(:course).permit(:topic, :price, :currency, :category, :is_available, :url, :content, :validity_period)
+  end
 end

5381d5eda841184912b7354e35050be921a6b4cf
lihsin <lihsin0427@gmail.com>
2022-02-13 20:33:48 +0800

    set currency

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,11 +1,14 @@
 class Course < ApplicationRecord
   # validation
-  validates :topic, :currency, :category, :content, presence: true
+  validates :topic, :category, :content, presence: true
 
   validates :price, presence: true, numericality: {
     greater_than_or_equal_to: 0
   }
   validate :price_is_valid_decimal_precision
+  
+  CURRENCY = %w(TWD USD JPY GBP EUR AUD HKD MOP MYR BEF CAD FRF ITL PHP DEM CHF SEK NZD SGD ESP DKK INR NOK NLG FIM SAR THB IDR ZAR ATS IEP MXN PLN CZK TRY HUF VND CNY)
+  validates :currency, presence: true, inclusion: { in: CURRENCY }
 
   validates :validity_period, presence: true, numericality: {
     greater_than_or_equal_to: 1,
@@ -26,10 +29,13 @@ class Course < ApplicationRecord
   private
   
   def price_is_valid_decimal_precision 
-    # Make sure that the rounded value is the same as the non-rounded 
     if price.to_f != price.to_f.round(2) 
      errors.add(:price, " should only be two digits at most after the decimal point.") 
     end 
   end 
 
 end
+
+
+
+

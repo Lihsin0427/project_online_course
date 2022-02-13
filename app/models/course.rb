acc2ed0df7b36222a233be36cf5bab619e9253a1
lihsin <lihsin0427@gmail.com>
2022-02-13 15:23:12 +0800

    basic validation in model Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,14 @@
 class Course < ApplicationRecord
+  # validation
+  validates :topic, :currency, :category, :url, :content, presence: true
+
+  validates :price, presence: true, numericality: {
+    greater_than_or_equal_to: 0
+  }
+
+  validates :validity_period, presence: true, numericality: {
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 31,
+    only_integer: true
+  }
 end
